0be1197c6782e6101a0c1679044d2e4d664273df
whereislijah <ayomideoduba@gmail.com>
2024-02-02 11:10:07 -0500

    Highest Bidder - day9

--- day7.py
+++ day7.py
@@ -2,80 +2,95 @@
 File: day7.py
 
 Description:
-    Caesar Cipher
+    Hangman
 
 Author: WhereIsLijah
 Version: 1.0
 Date: 2024-01-18
 """
-import string
 
-def caesar_cipher1(choice, plaintext, key):
-    if choice == 'decode':
-        key = -key
-    # creating a new list of where the cipher_text will be stored
-    cipher_text = []
+# - Generate random words
+# - Generate as many blanks
+# - Ask user to guess:
+#     - If letter is correct then fill up blanks
+#         repeat loop
+#     - If letter is wrong then reduce a life
+#
+# 6 incorrect guesses
 
-    # looping over all the characters in plaintext
-    for char in plaintext:
-        # checking if character is alphabet
-        if char.isalpha():
-            # setting the base of the Ascii values with values for both cases
-            ascii_offset = ord('a') if char.islower() else ord('A')
-            # creating the shift of characters by subtracting the current character from the offset i.e a/A
-            # and adding the key before using the modulus operator  by the number of letters of the alphabet
-            # and reconverting to the ascii value
-            shifted = ((ord(char) - ascii_offset + key) % 26) + ascii_offset
-            # converting the ascii value to character before storing the ascii character
-            cipher_text.append(chr(shifted))
+import random
+
+def word_list():
+    words = ["SLAPS", "COCONUT", "TREE", "LAPTOP"]
+    return random.choice(words)
+
+def create_blanks(chosen_word):
+    return ['_'] * len(chosen_word)
+
+def game_logic(word_entry, chosen_word, word_blank, life):
+    if len(word_blank) != 1 and not word_entry.isalpha():
+        print("Please enter a 'Single' Alphabet")
+        return life, word_blank
+
+    occurrences = find_occurrences(chosen_word, word_entry)
+    if not occurrences:
+        life -= 1
+
+        print(f"life is: {life}")
+        print(word_blank)
+        return life
+    else:
+        return fill_blank(word_entry, occurrences, word_blank, life)
+
+
+def find_occurrences(s, char):
+    return [index for index, c in enumerate(s) if c == char]
+
+
+def isListFilled(word_blank):
+    return '_' not in word_blank
+
+
+def fill_blank(word_entry, occurrences, word_blank, life):
+    print(f"fill_blank: {word_entry}")
+    for i in range(len(occurrences)):
+        if isListFilled(word_blank) is True:
+            print(word_blank)
+            life = 0
+            return life, 'Game Over!'
+        elif word_blank[occurrences[i]] == '_':
+            for i in range(len(occurrences)):
+                word_blank[occurrences[i]] = word_entry
+                print(word_blank)
+            return word_blank
         else:
-            # appending non-alphabets
-            cipher_text.append(char)
-    return ''.join(cipher_text)
+            return word_blank, "You already guessed that letter, try a different one!."
+    print(word_blank)
 
 
-def caesar_cipher(choice, plaintext, key):
-    # changing thr shift of the key if user's choice is to encode
-    if choice == 'decode':
-        key = -key
+def user_input(chosen_word, word_blank):
+    life = 6
+    while life > 0:
+        print(f"life remaining: {life}")
+        word_entry = input("Guess a letter: ").upper()
+        # word_guess(word_entry)
+        life, word_blank = game_logic(word_entry, chosen_word, word_blank, life)
 
-    # using a list comprehension to create a new list of lower alphabet letters [a, b, c,...]
-    alphabet_mapping = [letter for letter in string.ascii_lowercase]
 
-    # creating a new alphabet_hashmap to store letters and their corresponding indices
-    alphabet_hashmap = {}
-    for index, value in enumerate(alphabet_mapping):
-        alphabet_hashmap[value] = index
-    # creating an empty string variable to store the resulting encoded/decoded text
-    cipher_text = ""
+        if life == 0:
+            print("Game over!")
+            break
 
-    # Looping through the plaintext by each index and character
-    for index, value in enumerate(plaintext):
-        # checking if value is present in the alphabet hashmap
-        if value in alphabet_hashmap.keys():
-            # using mod to loop over the alphabet hashmap incase the shift key is higher than index 25
-            shift_key = (alphabet_hashmap[value] + key) % 26
-            # creates a new list of character found at a certain index if it is equal to the shift key
-            keys_with_value = ''.join([key for key, value in alphabet_hashmap.items() if value == shift_key])
-        else:
-            cipher_text += value
-        cipher_text += keys_with_value
-    return cipher_text
 
-# entry point of program, gets user inputs and calls a function to verify input
 def main():
-    try:
-        choice = str.strip(input("Do you want to Encode or Decode?\n").lower())
-        if choice not in ['encode', 'decode']:
-            raise ValueError
-
-        plaintext = input("Enter the Plaintext: ")
-        key = int(input("Enter the Shift Number: "))
-        print(f"The {choice}d text is: {caesar_cipher1(choice, plaintext, key)}")
-
-    except ValueError:
-        print(f'Error: Invalid input. Please ensure your choices are correct and the shift number is an integer.')
+    chosen_word = word_list()
+    word_blank = create_blanks(chosen_word)
+    print(chosen_word)
+    print(word_blank)
+    user_input(chosen_word, word_blank)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
+
+# convert game to ClI
---
+++ day9.py
@@ -0,0 +1,33 @@
+"""
+File: day9.py
+
+Description:
+    Secret Auction
+
+Author: WhereIsLijah
+Version: 1.0
+Date: 2024-01-18
+"""
+
+def main():
+    choice = True
+    bids = {}
+    while choice:
+        name = str.strip(input("What is your name?"))
+        bid = float(input("What's your bid?"))
+
+        bids[name] = bid
+
+        choice = input("Are there any other bidders? (yes/no) \n").lower()
+        if choice == 'no':
+            choice = False
+
+    max_bid = max(bids.values())
+
+    winner = ''.join([key for key, value in bids.items() if value == max_bid])
+
+    print(f"The winner is {winner} with a bid of {max_bid}")
+
+
+if __name__ == '__main__':
+    main()
