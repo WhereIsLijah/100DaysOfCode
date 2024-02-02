7a897504012c36823923ad8d35a2ecc676242f0e
whereislijah <ayomideoduba@gmail.com>
2024-02-01 20:56:36 -0500

    Update to Logic for Ceasar's cipher

--- day7.py
+++ day7.py
@@ -8,34 +8,46 @@ Author: WhereIsLijah
 Version: 1.0
 Date: 2024-01-18
 """
-
-# Program should Decode and Encode
-# Shift determines how the alphabets shift
-
-#create mapping for alphabets
-#get input and attach number to each letter
-#add key to each number of letter
-#find corresponding alpha
+#create ceaser cipher with a twist, put it in reverse
 
 
 import string
-def caesar_cipher(plaintext, key):
+def caesar_cipher(choice, plaintext, key):
     alphabet_mapping = [letter for letter in string.ascii_lowercase]
 
-    for i in plaintext:
-        alphabet_mapping
+    hashmap = {}
+    for index, value in enumerate(alphabet_mapping):
+        hashmap[value] = index
+    cipher_text = ""
 
+    if choice == 'decode':
+        key *= -1
+
+    for index, value in enumerate(plaintext):
+        if value in hashmap.keys():
+            shift_key = (hashmap[value] + key) % 26
+            keys_with_value = ''.join([key for key, value in hashmap.items() if value == shift_key])
+            cipher_text += keys_with_value
+    return cipher_text
+
+def user_choice(choice, plaintext, key):
+    if choice != 'encode' and choice != 'decode':
+        raise ValueError("Invalid Input")
+    else:
+        result = caesar_cipher(choice, plaintext, key)
+        return result
 
 def main():
-    # try:
-        # print("Do you want to Encode or Decode")
+    try:
+        choice = input("Do you want to Encode or Decode?\n").lower()
+        plaintext = input("Enter the Plaintext: ").lower()
+        key = int(input("Enter the Shift Number: "))
 
-    plaintext = input("Enter the Plaintext: ")
-    key = input("Enter the Shift Number: ")
+        print(f"The {choice}d text is: {user_choice(choice, plaintext, key)}")
 
-    caesar_cipher(plaintext, key)
 
-    # except:
+    except ValueError as e:
+        print(f"{e}, You can either Encode or decode!")
 
 
 
